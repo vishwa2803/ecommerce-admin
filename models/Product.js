c41d31a969170f1aa9f7abd6eb8016284a8bc813
vishwa satasiya <vishwasatasiya@gmail.com>
2024-04-03 17:52:42 +0530

    test

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,7 @@ const ProductSchema = new mongoose.Schema({
   title: {type:String, required:true},
   description:  { type: String, required: true },
   price: {type: Number, required: true}
-});
+})
 
 
 const productModel =mongoose.models.products || mongoose.model('products', ProductSchema);
